26f3cc53afdd531b6a83f1d8c44ee9b2144408dc
Jennifer Sung <sung.jenni93@gmail.com>
2023-05-16 04:10:25 -0400

    fixing voting button

--- js/app.js
+++ js/app.js
@@ -1,130 +1,125 @@
 'use strict';
 
-// GLOBALS  //
+// ***** GLOBALS ******
+let userVotes = 25;
+let productArray = [];
 
-let votingRounds = 25;
-let duckArray = [];
+
+
 
 // ***** DOM WINDOWS ****
-let imageContainer = document.getElementById('img-container');
-let imageOne = document.getElementById('img-one');
-let imageTwo = document.getElementById('img-two');
-let imageThree = document.getElementById('img-three');
-let resultButton = document.getElementById('show-results-btn');
-let resultList = document.getElementById('results-container');
+let imgContainer = document.getElementById('img-container');
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+let imgThree = document.getElementById('img-three');
+let resultBtn = document.getElementById('show-results-btn');
+let resultsList = document.getElementById('results-container');
+
+
+
 
 // **** CONSTRUCTOR FUNCTION ****
-
-function Duck(name, imageExtension = 'jpg') {
+function Product(name, imageExtension = 'jpg'){
   this.name = name;
   this.image = `img/${name}.${imageExtension}`;
   this.votes = 0;
   this.views = 0;
 }
 
+
+
 // **** HELPER FUNCTIONS / UTILITIES ****
-
-function randomIndexGenerator(){
-  return Math.floor(Math.random() * duckArray.length);
+function indexGenerator(){
+  return Math.floor(Math.random() * productArray.length);
 }
 
-// Three unique products
-function renderImages(){
-  let imageOneIndex = randomIndexGenerator();
-  let imageTwoIndex = randomIndexGenerator();
-  let imageThreeIndex = randomIndexGenerator();
+function renderImgs(){
+  let imageOneIndex = indexGenerator();
+  let imageTwoIndex = indexGenerator();
+  let imageThreeIndex = indexGenerator();
+
+  while(imageOneIndex === imageTwoIndex || imageOneIndex === imageThreeIndex || imageTwoIndex === imageThreeIndex){
+    imageTwoIndex = indexGenerator();
+    imageThreeIndex = indexGenerator();
+  }
+
+  imgOne.src = productArray[imageOneIndex].image;
+  imgOne.title = productArray[imageOneIndex].name;
+
+  imgTwo.src = productArray[imageTwoIndex].image;
+  imgTwo.title = productArray[imageTwoIndex].name;
+
+  imgThree.src = productArray[imageThreeIndex].image;
+  imgThree.title = productArray[imageThreeIndex].name;
+
+
+  productArray[imageOneIndex].views++;
+  productArray[imageTwoIndex].views++;
+  productArray[imageThreeIndex].views++;
 }
 
-// fix
-while(imageOneIndex === imageTwoIndex || imageOneIndex === imageThreeIndex || imageTwoIndex === imageThreeIndex){
-  imageTwoIndex = randomIndexGenerator();
-  imageThreeIndex = randomIndexGenerator();
-}
 
-//end fix
-
-imageOne.src = duckArray[imageOneIndex].image;
-imageOne.title = duckArray[imageOneIndex].name;
-
-imageTwo.src = duckArray[imageTwoIndex].image;
-imageTwo.title = duckArray[imageTwoIndex].name;
-
-imageThree.src = duckArray[imageThreeIndex].image;
-imageThree.title = duckArray[imageThreeIndex].name;
-
-// DONE: Increase the goats views
-duckArray[imageOneIndex].views++;
-duckArray[imageTwoIndex].views++;
-duckArray[imageThreeIndex].views++;
-}
 
 
 // **** EVENT HANDLERS ****
 function handleImgClick(event){
+  let imageClicked = event.target.title;
 
-let imageClicked = event.target.title;
+  for(let i = 0; i < productArray.length; i++){
+    if(imageClicked === productArray[i].name){
+      productArray[i].votes++;
+      userVotes--;
 
-for(let i = 0; i < duckArray.length; i++){
-  if(imageClicked === duckArray[i].name){
-    duckArray[i].votes++;
-    votingRounds--;
-    renderImages();
+      renderImgs();
+    }
   }
-}
-if(votingRounds === 0){
-  imageContainer.removeEventListener('click', handleImgClick);
-}
+
+
+  if(userVotes === 0){
+    imgContainer.removeEventListener('click', handleImgClick);
+  }
 
 }
 
 function handleShowResults(){
-if(votingRounds === 0){
-  for(let i = 0; i < goatArray.length; i++){
-    let duckListItem = document.createElement('li');
+  if(userVotes === 0){
+    for(let i = 0; i < productArray.length; i++){
+      let goatListItem = document.createElement('li');
 
-    duckListItem.textContent = `${duckArray[i].name} - Votes: ${duckArray[i].votes} & Views: ${duckArray[i].views}`;
+      goatListItem.textContent = `${productArray[i].name} - Votes: ${productArray[i].votes} & Views: ${productArray[i].views}`;
 
-    resultList.appendChild(duckListItem);
+      resultsList.appendChild(goatListItem);
+    }
+    resultBtn.removeEventListener('click', handleShowResults);
   }
-  resultButton.removeEventListener('click', handleShowResults);
-}
 }
 
-//
+// **** EXECUTABLE CODE *****
+let bag = new Product('bag');
+let banana = new Product('banana');
+let bathroom = new Product('bathroom');
+let boots = new Product('boots');
+let breakfast = new Product('breakfast');
+let bubblegum = new Product('bubblegum');
+let chair = new Product('chair');
+let cthulhu = new Product('cthulhu');
+let dogduck = new Product('dog-duck');
+let dragon = new Product('dragon');
+let pen = new Product('pen');
+let petsweep = new Product('pet-sweep');
+let scissors = new Product('scissors');
+let shark = new Product('shark');
+let sweep = new Product('sweep', 'png');
+let tauntaun = new Product('tauntaun');
+let unicorn = new Product('unicorn');
+let watercan = new Product('water-can');
+let wineglass = new Product('wine-glass');
 
 
-
-// **** EXECUTABLE CODE ****
-
-let bag = new Item('bag');
-let banana = new Item('banana');
-let bathroom = new Item('bathroom');
-let boots = new Item('boots');
-let breakfast = new Item('breakfast');
-let bubblegum = new Item('bubblegum');
-let chair = new Item('chair');
-let cthulhu = new Item('cthulhu');
-let dogDuck = new Item('dog-duck');
-let dragon = new Item('dragon');
-let pen = new Item('pen');
-let petSweep = new Item('pet-sweep');
-let scissors = new Item('scissors');
-let shark = new Item('shark');
-let sweep = new Item('sweep', 'png');
-let tauntaun = new Item('tauntaun');
-let unicorn = new Item('unicorn');
-let watercan = new Item('water-can');
-let wineglass = new Item('wine-glass');
-
-duckArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
-
-renderImages();
-
-imageContainer.addEventListener('click', handleImgClick);
-resultButton.addEventListener('click', handleShowResults);
+productArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
 
 
+renderImgs();
 
-
-
-
+imgContainer.addEventListener('click', handleImgClick);
+resultBtn.addEventListener('click', handleShowResults);
